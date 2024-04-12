a410424733d483472a24e11eac185447d78dc6cb
Constanza Sofía Alzatti <162724190+coalzatti@users.noreply.github.com>
2024-04-12 11:39:06 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,13 @@
 def leap_year():
-    print("TO DO")
+    
+    years=int(input("Ingrese un año: "))
+    if years%400 == 0:
+     print("El año"+" "+str(years)+" es bisiesto")
+    elif years%4==0 and years%100!=0:
+         print("El año"+" "+str(years)+" es bisiesto")
+    else:
+        print("El año"+" "+str(years)+" no es bisiesto")
+     
+
+leap_year()
+
